5620b71ed3bce5edbbd7f2a894448e311e98267b
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2026-07-07 12:24:20 -0400

    Bump golang.org/x/image to v0.43.0
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- go-mod-tidy-all.sh
+++ go-mod-tidy-all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 for i in $(find . -name go.mod); do
     echo $i
-    pushd ${i%go.mod} && go mod tidy && popd
+    pushd ${i%go.mod} && go get golang.org/x/image@latest && go mod tidy && popd
 done
